575689242b0c4153ea06adf723944f7f2a6e15d6
Herraj <herrajluhano@gmail.com>
2020-10-20 02:24:26 -0400

    Add core classes and methods

---
+++ src/common_wealth_assignment/Game.java
@@ -0,0 +1,24 @@
+package common_wealth_assignment;
+
+public class Game implements AnagramGame{
+
+	@Override
+	public void AnagrameGame(String letterset, String wordlist) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void submitWord(String word) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showHighScoreList() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
